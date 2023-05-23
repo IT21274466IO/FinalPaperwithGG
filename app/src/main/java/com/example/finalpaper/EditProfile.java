a06b144d5f0d983133d32af06d6989cf362ba382
IT21274466IO <it21274466@my.sliit.lk>
2023-05-23 12:08:44 +0530

    GG 2

--- app/src/main/java/com/example/finalpaper/EditProfile.java
+++ app/src/main/java/com/example/finalpaper/EditProfile.java
@@ -3,12 +3,94 @@ package com.example.finalpaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.finalpaper.Database.DBHandler;
+
+import java.util.List;
 
 public class EditProfile extends AppCompatActivity {
+
+    EditText username, dob, password;
+    Button edit, delete, search;
+    RadioButton male, female;
+    String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        username = findViewById(R.id.editTextTextPersonName);
+        dob = findViewById(R.id.etDobEP);
+        password = findViewById(R.id.etPasswordEP);
+        male = findViewById(R.id.radioButton3);
+        female = findViewById(R.id.radioButton4);
+        edit = findViewById(R.id.btnEditEP);
+        delete = findViewById(R.id.btnDeleteEP);
+        search = findViewById(R.id.btnSearchEP);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DBHandler dbHandler = new DBHandler(getApplicationContext());
+                List user = dbHandler.readAllInfo(username.getText().toString());
+
+                if (user.isEmpty()) {
+                    Toast.makeText(EditProfile.this, "No User Found", Toast.LENGTH_SHORT).show();
+                    username.setText(null);
+                } else {
+                    Toast.makeText(EditProfile.this, "User Found! User: " + user.get(0).toString(), Toast.LENGTH_SHORT).show();
+                    username.setText(user.get(0).toString());
+                    dob.setText(user.get(1).toString());
+                    password.setText(user.get(2).toString());
+                    if (user.get(3).toString().equals("Male")){
+                        male.setChecked(true);
+                    } else {
+                        female.setChecked(true);
+                    }
+                }
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(male.isChecked()){
+                    gender = "Male";
+                }else {
+                    gender = "Female";
+                }
+
+                DBHandler dbHandler = new DBHandler(getApplicationContext());
+                Boolean status = dbHandler.updateInfo(username.getText().toString(), dob.getText().toString(), password.getText().toString(), gender);
+                if (status) {
+                    Toast.makeText(EditProfile.this, "User Updated", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EditProfile.this, "Update Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHandler dbHandler = new DBHandler(getApplicationContext());
+                dbHandler.deleteInfo(username.getText().toString());
+                Toast.makeText(EditProfile.this, "User Deleted", Toast.LENGTH_SHORT).show();
+
+                username.setText(null);
+                dob.setText(null);
+                password.setText(null);
+                male.setChecked(false);
+                female.setChecked(false);
+            }
+        });
     }
 }
